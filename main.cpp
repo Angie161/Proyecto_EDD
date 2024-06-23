ffc6a89bd0867ff2e81684eb4c011483f139c8ea
zMatuiti <113061452+zMatuiti@users.noreply.github.com>
2024-06-22 21:17:50 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include <fstream>
 #include <string>
 #include <chrono>
-#include "CodHuffman.h"
+// #include "CodHuffman.h"
+#include "LZ24.h"
 
 int main(int argc, char const *argv[])
 {
-	std::ifstream file("dna.5MB.txt");
+	std::ifstream file("dna.10MB.txt");
 	if (!file.is_open())
 	{
 		std::cerr << "Error al abrir el archivo " << std::endl;
@@ -20,29 +21,54 @@ int main(int argc, char const *argv[])
 		text += line;
 	}
 
-	file.close();
+	// ---------------------- HUFFMAN -----------------------------------
 
+	// Medir el tiempo de codificacion
+	// auto start_Huffman = std::chrono::high_resolution_clock::now(); // Tiempo de inicio
+	// ArbolCompress_Huffman arbol_Huffman(text);
+	// auto end_Huffman = std::chrono::high_resolution_clock::now(); // Tiempo de finalización
+	// // Calcular la duración
+	// auto duration_Huffman = std::chrono::duration_cast<std::chrono::milliseconds>(end_Huffman - start_Huffman);
+	// std::cout << "Huffman" << ";10MB" << ";codificacion" << ";"  << arbol_Huffman.getSizeCode() << ";" << duration_Huffman.count() << std::endl;
+
+	// // Medir el tiempo de decodificacion
+	// auto startDes_Huffman = std::chrono::high_resolution_clock::now(); // Tiempo de inicio
+	// arbol_Huffman.decodeTree();
+	// auto endDes_Huffman = std::chrono::high_resolution_clock::now(); // Tiempo de finalización
+	// // Calcular la duración
+	// auto durationDes_Huffman = std::chrono::duration_cast<std::chrono::milliseconds>(endDes_Huffman - startDes_Huffman);
+	// std::cout << "Huffman" << ";10MB" << ";decodificacion" << ";" << arbol_Huffman.getSizeDeCode() << ";" << durationDes_Huffman.count() << std::endl;
+
+	// ---------------------- LEMPEL ZIV -----------------------------------
 	// Medir el tiempo de compresión
-	auto start = std::chrono::high_resolution_clock::now(); // Tiempo de inicio
-
-	ArbolCompress arbol(text);
-
-	auto end = std::chrono::high_resolution_clock::now(); // Tiempo de finalización
-
+	auto start_LempelZiv = std::chrono::high_resolution_clock::now(); // Tiempo de inicio
+	LZ24 arbol_LempelZiv;
+	arbol_LempelZiv.compress("dna.5MB.txt", "compressed.lz24");
+	auto end_LempelZiv = std::chrono::high_resolution_clock::now(); // Tiempo de finalización
 	// Calcular la duración
-	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-	std::cout << "Huffman" << ";" << arbol.getSizeCodeBit() << ";" << duration.count() << std::endl;
+	auto duration_LempelZiv = std::chrono::duration_cast<std::chrono::milliseconds>(end_LempelZiv - start_LempelZiv);
+
+	// Obtener el tamaño del archivo comprimido
+	std::ifstream compressFile("compressed.lz24", std::ios::binary | std::ios::ate);
+	std::streamsize compressedSize = compressFile.tellg();
+	compressFile.close();
+
+	std::cout << "LempelZiv" << ";10MB" << ";compresion" << ";" << compressedSize << ";" << duration_LempelZiv.count() << std::endl;
 
 	// Medir el tiempo de descompresión
-	auto startDes = std::chrono::high_resolution_clock::now(); // Tiempo de inicio
-
-	arbol.decodeTree();
-
-	auto endDes = std::chrono::high_resolution_clock::now(); // Tiempo de finalización
-
+	auto startDes_LempelZiv = std::chrono::high_resolution_clock::now(); // Tiempo de inicio
+	arbol_LempelZiv.decompress("compressed.lz24", "decompressed.txt");
+	auto endDes_LempelZiv = std::chrono::high_resolution_clock::now(); // Tiempo de finalización
 	// Calcular la duración
-	auto durationDes = std::chrono::duration_cast<std::chrono::milliseconds>(endDes - startDes);
-	std::cout << "Huffman" << ";" << arbol.getSizeDeCode() << ";" << durationDes.count() << std::endl;
+	auto durationDes_LempelZiv = std::chrono::duration_cast<std::chrono::milliseconds>(endDes_LempelZiv - startDes_LempelZiv);
 
+	// Obtener el tamaño del archivo descomprimido
+	std::ifstream decompressedFile("decompressed.txt", std::ios::binary | std::ios::ate);
+	std::streamsize decompressedSize = decompressedFile.tellg();
+	decompressedFile.close();
+
+	std::cout << "LempelZiv" << ";10MB" << ";descompresion" << ";" << decompressedSize << ";" << durationDes_LempelZiv.count() << std::endl;
+
+	file.close();
 	return 0;
 }
